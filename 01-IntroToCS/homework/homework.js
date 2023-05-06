07f69398c20955145b01e31593b695d6abd53ffc
Leogriffo <leandro.griffone@gmail.com>
2023-05-06 14:30:51 -0300

    second commit

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,8 +1,24 @@
 'use strict';
 
-function BinarioADecimal(num) {}
+function BinarioADecimal(num) {
 
-function DecimalABinario(num) {}
+   var array = num.split("")//divide el string en pequeños array
+   var sum = 0;
+   for (var i = 0; i< array.length; i++){
+      sum = sum + Math.pow(2,array.length -1-i)* array[i]// math.pow retorna la base elevada al exponente
+   }
+   return sum;
+}
+
+function DecimalABinario(num) {
+   var resultado = "";
+   
+   while(num !==0){
+      resultado = (num%2) + resultado
+      num = Math.floor(num/2)
+   }
+   return resultado;
+}
 
 module.exports = {
    BinarioADecimal,
